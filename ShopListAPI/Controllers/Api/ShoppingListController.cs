8137d7c34b4c9ae050a5dfb97c5bb24d72f0f13b
Adrien <Adrien@ADRIEN-PC>
2016-08-01 20:17:52 +0400

    cotroller mockup ready

---
+++ ShopListAPI/Controllers/Api/ShoppingListController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ShopListAPI.Dtos;
+
+namespace ShopListAPI.Controllers.Api
+{
+    public class ShoppingListController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<ShopItemDto> GetAllShopItems() 
+        {
+            List<ShopItemDto> items;
+            items = new List<ShopItemDto>();
+            //TODO: get the list from the repo->database...
+            return items;
+        }
+
+        [HttpGet]
+        public ShopItemDto GetShopItemById(string id) 
+        {
+            //TODO: get a specific shopping list item... base on the id provided...
+            var item = new ShopItemDto
+            {
+                ItemName = "pepsi",
+                ItemQuantity = 1
+            };
+            return item;
+        }
+
+        [HttpPost]
+        public IHttpActionResult AddShopItem(ShopItemDto item)
+        {
+            //TODO: add the item passed via parameter to the repo...
+            return Ok();
+        } // add an item to the shopping list...
+
+        [HttpPut]
+        public IHttpActionResult FullUpdateShopItem(string itemId, ShopItemDto item)
+        {
+            //TODO: Insert the whole Item....
+            return Ok(item);
+        }
+
+        [HttpPatch]
+        public IHttpActionResult DeltaUpdateShopItem(string itemId, ShopItemDto item)
+        {
+            //TODO: Insert the partial Item....
+            return Ok(item);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteShopItem(string id)
+        {
+            //TODO: delete shop item from repo...
+            return NotFound();
+        }
+    }
+}
---
+++ ShopListAPI/Dtos/ShopItemDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopListAPI.Dtos
+{
+    public class ShopItemDto
+    {
+        public string ItemName { get; set; }
+        public int ItemQuantity { get; set; }
+        public double UnitPrice { get; set; }
+        public double TotalPrice { get; set; }
+        public bool CheckedOut { get; set; }
+    }
+}
